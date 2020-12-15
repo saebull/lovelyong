cac90e84264a160864cba5149272990a159f9528
saebull <73066922+saebull@users.noreply.github.com>
2020-12-15 19:01:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,222 +12,32 @@ cred = credentials.Certificate('ongkey.json')
 defaul_app = firebase_admin.initialize_app(cred,{
     'databaseURL' : 'https://ie-project-292614.firebaseio.com/'
 })
-keyname = ['강남대로1',
-'강남대로2',
-'강남대로3',
-'강남대로4',
-'강남대로5',
-'강남대로6',
-'강남대로7',
-'강남대로8',
-'강남대로8_9',
-'강남대로9',
-'강남대로91길1',
-'강남대로91길2',
-'강남대로93길1',
-'강남대로93길2',
-'강남대로93길3',
-'강남대로93길3_4',
-'강남대로93길4',
-'강남대로93길4_5',
-'강남대로93길5',
-'강남대로95길1',
-'강남대로95길1_2_1',
-'강남대로95길1_2_2',
-'강남대로95길2',
-'강남대로95길3',
-'강남대로95길4',
-'강남대로95길4_5_1',
-'강남대로95길4_5_2',
-'강남대로95길5',
-'강남대로95길6',
-'강남대로95길7',
-'강남대로95길8',
-'강남대로97길1',
-'강남대로97길2',
-'강남대로97길3',
-'강남대로97길4',
-'강남대로97길5',
-'강남대로97길5_6_1',
-'강남대로97길5_6_2',
-'강남대로97길6',
-'강남대로99길1',
-'강남대로99길2',
-'강남대로99길2_3',
-'강남대로99길3',
-'강남대로99길3_4',
-'강남대로99길4',
-'강남대로99길5',
-'강남대로100길1',
-'강남대로100길2',
-'강남대로100길3',
-'강남대로100길4',
-'강남대로100길5',
-'강남대로101안길1',
-'강남대로101안길2',
-'강남대로101안길3',
-'강남대로101안길4',
-'강남대로105길1',
-'강남대로105길2',
-'강남대로109길1',
-'강남대로109길2',
-'강남대로109길3',
-'강남대로109길4',
-'강남대로109길4_5_1',
-'강남대로109길4_5_2',
-'강남대로109길5',
-'강남대로109길6',
-'나루터로1',
-'나루터로2',
-'나루터로2_3',
-'나루터로3',
-'나루터로4',
-'나루터로5',
-'나루터로6',
-'나루터로7',
-'나루터로8',
-'나루터로4길1',
-'나루터로4길2',
-'나루터로4길3',
-'나루터로4길4',
-'나루터로4길4_5',
-'나루터로4길5',
-'나루터로8길1',
-'나루터로8길2',
-'나루터로8길3',
-'나루터로8길4',
-'나루터로10길1',
-'나루터로10길2',
-'나루터로10길3',
-'나루터로12길1',
-'나루터로12길2',
-'나루터로12길3',
-'나루터로12길3_4_1',
-'나루터로12길3_4_2',
-'나루터로12길3_4_3',
-'나루터로12길4',
-'나루터로15길1',
-'나루터로15길2',
-'나루터로15길3',
-'신반포로1',
-'신반포로2',
-'신반포로3',
-'신반포로4',
-'신반포로5',
-'신반포로6',
-'신반포로7',
-'신반포로33길1',
-'신반포로33길2',
-'신반포로33길3',
-'신반포로33길4',
-'신반포로33길4_5',
-'신반포로33길5',
-'신반포로33길5_6',
-'신반포로33길6',
-'신반포로33길6_7',
-'신반포로33길7',
-'신반포로41길1',
-'신반포로41길2',
-'신반포로41길3',
-'신반포로41길4',
-'신반포로41길5',
-'신반포로43길1',
-'신반포로43길1_2_1',
-'신반포로43길1_2_2',
-'신반포로43길2',
-'신반포로43길2_3',
-'신반포로43길3',
-'신반포로43길3__1',
-'신반포로43길3__2',
-'신반포로45길1',
-'신반포로45길2',
-'신반포로45길4',
-'신반포로45길5',
-'신반포로45길6',
-'신반포로45길6_7',
-'신반포로45길7',
-'신반포로45길7_8',
-'신반포로45길8',
-'신반포로47길1',
-'신반포로47길2',
-'신반포로47길3',
-'신반포로47길4',
-'신반포로47길5',
-'신반포로47길6',
-'신반포로47길7',
-'신반포로47길8',
-'신반포로47길9',
-'신반포로47길10',
-'신반포로47길10_11',
-'신반포로47길11',
-'신반포로47길11_12',
-'신반포로47길12',
-'신반포로47길12_13',
-'신반포로47길13',
-'신반포로49길1',
-'신반포로49길2',
-'잠원로1',
-'잠원로2',
-'잠원로2_3_1',
-'잠원로2_3_2',
-'잠원로3',
-'잠원로4',
-'잠원로5',
-'잠원로5_6',
-'잠원로6',
-'잠원로7',
-'잠원로7_8_1',
-'잠원로7_8_2',
-'잠원로7_8_3',
-'잠원로8',
-'잠원로4길1',
-'잠원로4길1_2_1',
-'잠원로4길1_2_2',
-'잠원로4길1_2_3',
-'잠원로4길1_2_3_1',
-'잠원로4길1_2_4',
-'잠원로4길2',
-'잠원로4길3',
-'잠원로8길1',
-'잠원로8길2',
-'잠원로8길3',
-'잠원로8길4',
-'잠원로12길1',
-'잠원로12길2',
-'잠원로12길3',
-'잠원로12길4',
-'잠원로12길5',
-'잠원로14길1',
-'잠원로14길2']
-lat = db.reference('/lat').get()
-lng = db.reference('/lng').get()
-p1lat = db.reference('/p1lat').get()
-p1lng = db.reference('/p1lng').get()
-p2lat = db.reference('/p2lat').get()
-p2lng = db.reference('/p2lng').get()
 
-latd = []
-lngd = []
-p1latd = []
-p1lngd = []
-p2latd = []
-p2lngd = []
-for i in keyname :
-    latd.append(lat[i])
-    lngd.append(lng[i])
-    p1latd.append(p1lat[i])
-    p1lngd.append(p1lng[i])
-    p2latd.append(p2lat[i])
-    p2lngd.append(p2lng[i])
-roaddf = pd.DataFrame({'도로명':list(keyname)})
-ddff = pd.DataFrame({'p1위도':p1latd, 'p1경도':p1lngd,
-                       'p2위도':p2latd,'p2경도':p2lngd})
+keyname = db.reference('/p1lat').get().keys()
+p1lat = db.reference('/p1lat').get().values()
+p1lng = db.reference('/p1lng').get().values()
+p2lat = db.reference('/p2lat').get().values()
+p2lng = db.reference('/p2lng').get().values()
+lat = db.reference('/lat').get().keys()
+lng = db.reference('/lng').get().keys()
+df = pd.DataFrame({'p1위도':list(p1lat),'p1경도':list(p1lng),
+                       'p2위도':list(p2lat),'p2경도':list(p2lng)})
+df2 = pd.DataFrame({'도로명':list(keyname)})
+df3 = pd.DataFrame({'도로명':list(keyname),'위도':list(lat),'경도':list(lng)})
+ref = db.reference('/road')
+lst = ref.get()
 
-ref = db.reference('/finalscore')
-lst = ref.get() ## finalscore
-ref_road = db.reference('/road')
-lst2 = ref_road.get()
-df3 = pd.DataFrame({'도로명':keyname, '위도':latd,'경도':lngd})
+score = db.reference('/score')
+sc = score.get()
+key = sc.keys()
+value = sc.values()
+keylst=[]
+for r_n in key:
+    keylst.append(r_n)
+sc_lst = []
+for i in value:
+    sc_lst.append(i['score'])
+data = pd.DataFrame({'도로명':keylst,'합계':sc_lst})
 
 def getlatlng(address):
     base_url = "https://maps.googleapis.com/maps/api/geocode/xml?address="
@@ -239,19 +49,17 @@ def getlatlng(address):
     lat = float(lat.replace('[', '').replace(']', ''))
     lng = float(lng.replace('[', '').replace(']', ''))
     return lat,lng
+
 def getPos(x, y):
     x = float(x)
     y = float(y)
     position1=[]
     position2 =[]
-    for i in range(len(ddff)):
-        if abs(x - ddff.loc[i, 'p1위도']) + abs(y - ddff.loc[i, 'p1경도']) >\
-                abs(x - ddff.loc[i, 'p2위도']) + abs(y - ddff.loc[i, 'p2경도']):
-
-            position1.append([i, abs(x - ddff.loc[i, 'p1위도']) + abs(y - ddff.loc[i, 'p1경도'])])
+    for i in range(len(df)):
+        if abs(x - df.loc[i,'p1위도']) + abs(y - df.loc[i,'p1경도']) > abs(x - df.loc[i,'p2위도']) + abs(y - df.loc[i,'p2경도']):
+            position1.append([i,abs(x - df.loc[i,'p1위도']) + abs(y - df.loc[i,'p1경도'])])
         else:
-            position2.append([i, abs(x - ddff.loc[i, 'p2위도']) + abs(y - ddff.loc[i, 'p2경도'])])
-
+            position2.append([i,abs(x - df.loc[i,'p2위도']) + abs(y - df.loc[i,'p2경도'])])
     minn = 1000
     result1 =0
     result2 = 0
@@ -267,54 +75,11 @@ def getPos(x, y):
                 result2 = j[0]
 
     if result1 >0:
-        road =df3.loc[result1, '도로명']
-
+        road =df2.loc[result1,'도로명']
         return road
     else :
-        road =df3.loc[result2, '도로명']
-
+        road =df2.loc[result2,'도로명']
         return road
-
-def allpath(graph, start, end):
-    visited = {start: 0}
-    h = [(0, start)]
-    path = {}
-    lst=[]
-    distances = {vertex: float('inf') for vertex in graph} # 시작점과 모든 정점과의 사리의 거리를 무한으로 지정
-    while distances:
-        current_distance, current_vertex = heapq.heappop(h)
-        try:
-            while current_vertex not in distances:
-                current_distance, current_vertex = heapq.heappop(h)
-        except IndexError:
-             break
-
-        if current_vertex == end:
-            way = end
-            lst.append(way)
-            path_output = end + '->'
-            while path[way] != start:
-                path_output += path[way] + '->'
-                way = path[way]
-                lst.append(way)
-            lst.append(start)
-            path_output += start
-
-            return visited[end], path, lst
-
-        del distances[current_vertex]
-
-        for v, weihgt in graph[current_vertex].items():
-            weihgt = current_distance + weihgt
-
-
-            if v not in visited or weihgt < visited[v] :
-                visited[v] = weihgt
-                heapq.heappush(h,(weihgt,v))
-                path[v] = current_vertex
-
-
-    return visited,path,lst
 
 def dijkstra(graph, start, end):
     visited = {start: 0}
@@ -346,7 +111,7 @@ def dijkstra(graph, start, end):
                 lst.append(way)
             lst.append(start)
             path_output += start
-            #print(path_output)
+            print(path_output)
 
             return visited[end], path, lst
 
@@ -365,11 +130,45 @@ def dijkstra(graph, start, end):
 
 
     return visited,path,lst
-def findlatlng(name):
-    lat = df3[df3['도로명'] == name].loc[:, '위도'].values[0]
-    lng = df3[df3['도로명'] == name].loc[:, '경도'].values[0]
-    return lat, lng
 
+def findlatlng(name):
+    lat = df3[df3['도로명']==name].loc[:,'위도'].values
+    lng = df3[df3['도로명']==name].loc[:,'경도'].values
+    return lat,lng
+
+total = []
+def dfs_paths(graph, start, end, visited=[],weight = [0]):
+    visited = visited + [start]
+
+    if len(visited) > 1 :
+        a = lst[visited[-2]][start]
+        weight = weight + [a]
+    # 도착할 경우, paths에 경로를 기록한다.
+    if start == end:
+        total.append([visited,sum(weight)])
+        #paths.append(visited)
+        #totalweight.append(sum(weight))
+
+    # 현재 노드의 자손 노드들 중, 방문하지 않은 노드들에 대해 재귀 호출
+    for node, k in graph[start].items():
+        if node not in visited:
+            if len(visited) >= 10:
+                continue
+            dfs_paths(graph, node, end, visited, weight)
+    total.sort(key=lambda x: x[1])
+
+    return total
+def calscore(lst):
+    temp = []
+    for i, j in lst[0:3]:
+        score = 0
+        for h in i:
+            for index,k in enumerate(data['도로명']):
+                if h == k:
+                    score += data.loc[index,'합계']
+        temp.append([i,score,j])
+    temp.sort(key=lambda x: x[1])
+    return temp[0][0], temp[0][2]
 
 app = Flask(__name__)
 
@@ -386,14 +185,13 @@ def result():
        des_lat, des_lng = getlatlng(des)
        s = getPos(dep_lat, dep_lng)
        e = getPos(des_lat, des_lng)
-       dfs_a,dfs_b,dfs_c = allpath(lst,s,e)
+       dfs = dfs_paths(lst,s,e)
        temp = []
-
-       for i in dfs_c:
+       b,c = calscore(dfs)
+       for i in b:
            ss,ee = findlatlng(i)
-           temp.append((ss,ee))
+           temp.append((ss[0],ee[0]))
 
-       temp.reverse()
 
        return render_template("generic.html", dep=dep, des=des, dep_lat=dep_lat, dep_lng=dep_lng, des_lat=des_lat, des_lng=des_lng, temp=temp)
 
@@ -406,11 +204,11 @@ def result2():
        des_lat, des_lng = getlatlng(des)
        s = getPos(dep_lat, dep_lng)
        e = getPos(des_lat, des_lng)
-       (a, b, c) = dijkstra(lst2, s, e)
+       (a, b, c) = dijkstra(lst, s, e)
        temp = []
        for i in c:
            ss,ee = findlatlng(i)
-           temp.append((ss,ee))
+           temp.append((ss[0],ee[0]))
        temp.reverse()
        return render_template("elements.html", dep_lat=dep_lat, dep_lng=dep_lng, des_lat=des_lat, des_lng=des_lng, temp=temp)
 
@@ -428,4 +226,4 @@ def generic():
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug = True )
+    app.run(host = '0.0.0.0', debug = True)
